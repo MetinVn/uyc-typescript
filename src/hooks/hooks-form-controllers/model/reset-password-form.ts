6efb9aa4e29528e14ab5279a4109a21c3a29d726
Meto <misaxanli@gmail.com>
2025-08-08 00:07:15 +0400

    optimized performance

--- src/hooks/hooks-form-controllers/model/reset-password-form.ts
+++ src/hooks/hooks-form-controllers/model/reset-password-form.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 import { useFormController } from "../controller/form-controller";
 
+import { useMemo } from "react";
+
 const forgotPassSchema = z.object({
   email: z.string().email("Invalid email address"),
 });
 export type ForgotPasswordData = z.infer<typeof forgotPassSchema>;
 
-export function useForgotPasswordForm() {
-  return useFormController<ForgotPasswordData>(forgotPassSchema, {
-    email: "",
-  });
-}
+export const useForgotPasswordForm = () => {
+  const initialValues = useMemo(() => ({ email: "" }), []);
+
+  return useFormController<ForgotPasswordData>(forgotPassSchema, initialValues);
+};
