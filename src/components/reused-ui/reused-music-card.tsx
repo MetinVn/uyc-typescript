73aee1908d664e38dfac510b133717eb791591cd
Meto <misaxanli@gmail.com>
2025-08-07 23:59:08 +0400

    new icons && performance optimizations && new option to copy the song link

--- src/components/reused-ui/reused-music-card.tsx
+++ src/components/reused-ui/reused-music-card.tsx
@@ -1,12 +1,14 @@
-import { useRef, useState } from "react";
+import { memo, useCallback, useMemo, useRef, useState } from "react";
 import { motion } from "framer-motion";
-import { FaStar, FaRegStar } from "react-icons/fa";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { Star } from "lucide-react";
+import { EllipsisVertical } from "lucide-react";
+
 import { Music } from "../../types/types-converted-music";
+import { useHandleOutsideClicks } from "../../hooks/hook-outside-clicks";
 import { formatDuration } from "../../utils/format-music-duration";
 import { formatSize } from "../../utils/format-music-size";
-import { useHandleOutsideClicks } from "../../hooks/hook-outside-clicks";
-import ImageLoader from "../../utils/img-loader";
+import { notify } from "../../stores/shared/notification";
+import { ImageLoader } from "../../utils/img-loader";
 
 interface IMusicCard {
   music: Music;
@@ -15,7 +17,7 @@ interface IMusicCard {
   onRemove: (music: Music) => void;
 }
 
-export const MusicCard = ({ music, onRate, onToggleFavorite, onRemove }: IMusicCard) => {
+export const MusicCard = memo(({ music, onRate, onToggleFavorite, onRemove }: IMusicCard) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const cardRef = useRef<HTMLDivElement>(null);
@@ -23,8 +25,32 @@ export const MusicCard = ({ music, onRate, onToggleFavorite, onRemove }: IMusicC
   useHandleOutsideClicks({
     isActive: isMenuOpen,
     ref: cardRef,
-    stateChanger: () => setIsMenuOpen(false),
+    stateChanger: setIsMenuOpen,
   });
+
+  const formattedDuration = useMemo(() => {
+    return formatDuration(music.duration);
+  }, [music.duration]);
+
+  const formattedSize = useMemo(() => {
+    return formatSize(music.fileSize);
+  }, [music.fileSize]);
+
+  const handleFavorite = useCallback(() => {
+    onToggleFavorite(music);
+    setIsMenuOpen(false);
+  }, []);
+
+  const copyLink = useCallback(() => {
+    navigator.clipboard.writeText(music.youtubeLink);
+    setIsMenuOpen(false);
+    notify.success("Link copied to clipboard!", 1500);
+  }, []);
+
+  const handleRemove = useCallback(() => {
+    onRemove(music);
+    setIsMenuOpen(false);
+  }, []);
 
   return (
     <motion.li
@@ -42,7 +68,7 @@ export const MusicCard = ({ music, onRate, onToggleFavorite, onRemove }: IMusicC
       <div className="relative w-full h-45 sm:h-50 rounded-lg overflow-hidden">
         <ImageLoader className="w-full h-full object-cover object-center" alt={music.title} imgSrc={music.thumbnail} />
         <p className="absolute bottom-1 right-1 bg-[var(--gray-900)]/60 text-[var(--gray-100)] text-[10px] sm:text-xs px-2 py-0.5 rounded">
-          {formatDuration(music.duration)}
+          {formattedDuration}
         </p>
       </div>
 
@@ -52,27 +78,29 @@ export const MusicCard = ({ music, onRate, onToggleFavorite, onRemove }: IMusicC
             {music.title}
           </h2>
           <div className="flex items-center gap-1">
-            <p className="text-[10px] sm:text-sm text-[var(--gray-400)] mt-1">Size: {formatSize(music.fileSize)}</p>
+            <p className="text-[10px] sm:text-sm text-[var(--gray-400)] mt-1">Size: {formattedSize}</p>
             <p className="text-[10px] sm:text-sm text-[var(--gray-400)] mt-1">
               Starred: {music.starred ? "Yes" : "No"}
             </p>
           </div>
 
           <div className="flex items-center gap-1 mt-2">
-            {[1, 2, 3, 4, 5].map((n) => (
-              <button
-                key={n}
-                onClick={() => onRate(n, music)}
-                className="p-1 cursor-pointer rounded hover:fill-[var(--yellow-400)] text-[10px] sm:text-xs transition"
-                title={`Rate ${n} star${n > 1 ? "s" : ""}`}
-              >
-                {n <= music.rating ? (
-                  <FaStar className="text-[var(--yellow-400)]" />
-                ) : (
-                  <FaRegStar className="text-[var(--gray-400)]" />
-                )}
-              </button>
-            ))}
+            {[1, 2, 3, 4, 5].map((n) => {
+              const isFilled = n <= music.rating;
+
+              const starColor = isFilled ? "var(--yellow-400)" : "var(--gray-400)";
+
+              return (
+                <button
+                  key={n}
+                  onClick={() => onRate(n, music)}
+                  className="p-1 cursor-pointer rounded hover:fill-[var(--yellow-400)] text-[10px] sm:text-xs transition"
+                  title={`Rate ${n} star${n > 1 ? "s" : ""}`}
+                >
+                  <Star size={15} fill={isFilled ? starColor : "none"} stroke={starColor} className="transition" />
+                </button>
+              );
+            })}
           </div>
         </div>
 
@@ -82,38 +110,40 @@ export const MusicCard = ({ music, onRate, onToggleFavorite, onRemove }: IMusicC
             onClick={() => setIsMenuOpen((prev) => !prev)}
             className="active:bg-[var(--gray-900)]/20 hover:bg-[var(--gray-900)]/30 cursor-pointer rounded-full p-2"
           >
-            <BsThreeDotsVertical size={18} />
+            <EllipsisVertical size={18} />
           </button>
 
           <div
-            className={` absolute overflow-hidden right-3 -top-[70px] w-32 min-w-[140px] z-50 bg-[var(--gray-800)] rounded-md shadow-lg flex flex-col text-xs text-left text-[var(--gray-100)] transform duration-200 ease-in-out ${
+            className={` absolute overflow-hidden right-3 -top-[90px] w-32 min-w-[140px] z-50 bg-[var(--gray-800)] rounded-md shadow-lg flex flex-col text-xs text-left text-[var(--gray-100)] transform duration-200 ease-in-out ${
               isMenuOpen ? "opacity-100 visible scale-100" : "opacity-0 invisible scale-80 pointer-events-none"
             }`}
           >
             <button
               type="button"
-              onClick={() => {
-                onToggleFavorite(music);
-                setIsMenuOpen(false);
-              }}
+              onClick={handleFavorite}
               className="hover:bg-[var(--gray-700)] text-left cursor-pointer p-2 transition"
             >
               {music.starred ? "Remove from favorites" : "Add to favorites"}
             </button>
 
             <button
+              className="hover:bg-[var(--gray-700)] text-left cursor-pointer p-2 transition"
               type="button"
-              onClick={() => {
-                onRemove(music);
-                setIsMenuOpen(false);
-              }}
+              onClick={copyLink}
+            >
+              Copy Link
+            </button>
+
+            <button
+              type="button"
+              onClick={handleRemove}
               className="hover:bg-[var(--red-500)]/30 text-left cursor-pointer p-2 text-[var(--red-200)] transition"
             >
-              Delete
+              Remove
             </button>
           </div>
         </div>
       </div>
     </motion.li>
   );
-};
+});
