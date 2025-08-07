67862e3f9bfba85d7ba048a2f5a664ff842e2e72
Meto <misaxanli@gmail.com>
2025-08-08 00:00:45 +0400

    small changes && performance optimization

--- src/components/user-ui/home-page-profile.tsx
+++ src/components/user-ui/home-page-profile.tsx
@@ -1,30 +1,35 @@
-import { useRef, useState } from "react";
-import placeholder from "../../images/profilePic.avif";
-import { ROUTES } from "../../routes/routes";
-import { DangerButton } from "../reused-ui/reused-button";
-import { CustomLink } from "../reused-ui/reused-router-link";
+import { memo, useCallback, useRef, useState } from "react";
 import { User } from "firebase/auth";
+
+import { ROUTES } from "../../routes/routes";
+import { useMusicFormat } from "../../stores/shared/format-change";
 import { useHandleOutsideClicks } from "../../hooks/hook-outside-clicks";
 import { signOutCurrentUser } from "../../services/user/firebase";
-import { notify } from "../../stores/shared/notification";
-import { useMusicFormat } from "../../stores/shared/format-change";
 import { converted } from "../../stores/shared/converted-song";
+import { notify } from "../../stores/shared/notification";
+import { CustomLink } from "../reused-ui/reused-router-link";
+import { DangerButton } from "../reused-ui/reused-button";
+import { ImageLoader } from "../../utils/img-loader";
 
-export const UserProfile = ({ user }: { user: User }) => {
+export const UserProfile = memo(({ user }: { user: User }) => {
   const [expand, setExpand] = useState(false);
-  const [imgSrc, setImgSrc] = useState(user.photoURL || placeholder);
   const [confirmLogOut, setConfirmLogOut] = useState(false);
-  const handleToggle = () => setExpand((prev) => !prev);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const confirmLogOutRef = useRef<HTMLDivElement>(null);
 
   const resetFormat = useMusicFormat((state) => state.resetFormat);
   useHandleOutsideClicks({ isActive: expand, ref: dropdownRef, stateChanger: setExpand });
+  useHandleOutsideClicks({ isActive: confirmLogOut, ref: confirmLogOutRef, stateChanger: setConfirmLogOut });
 
-  const handleLogOutPopup = () => {
-    setConfirmLogOut(true);
-  };
+  const handleToggle = useCallback(() => {
+    setExpand((prev) => !prev);
+  }, []);
 
-  const handleSignOut = async () => {
+  const handleLogOutPopup = useCallback(() => {
+    setConfirmLogOut((prev) => !prev);
+  }, []);
+
+  const handleSignOut = useCallback(async () => {
     const result = await signOutCurrentUser();
     if (result) {
       resetFormat();
@@ -35,7 +40,7 @@ export const UserProfile = ({ user }: { user: User }) => {
     }
     notify.error("Unexpected error happened while signing you out, please try again.", 2500);
     return false;
-  };
+  }, []);
 
   return (
     <div ref={dropdownRef} className="relative w-fit sm:mr-20">
@@ -44,13 +49,7 @@ export const UserProfile = ({ user }: { user: User }) => {
         onClick={handleToggle}
         className="hidden sm:flex items-center gap-1 p-1 rounded-full cursor-pointer bg-[var(--gray-800)] hover:opacity-90 active:bg-[var(--gray-700)] hover:bg-[var(--gray-700)] transition"
       >
-        <img
-          src={imgSrc}
-          alt="profile picture"
-          draggable={false}
-          onError={() => setImgSrc(placeholder)}
-          className="w-15 h-15 rounded-full object-cover object-center"
-        />
+        <ImageLoader imgSrc={user.photoURL} className="w-15 h-15 rounded-full object-cover object-center" />
         <div className="flex flex-col items-start px-2 text-left">
           <h1 className="text-sm max-w-[125px] truncate text-[var(--gray-100)]">{user?.displayName || "User"}</h1>
           <p className="text-xs max-w-[135px] truncate text-[var(--gray-400)]">{user?.email || "example@gmail.com"}</p>
@@ -62,12 +61,7 @@ export const UserProfile = ({ user }: { user: User }) => {
         onClick={handleToggle}
         className="sm:hidden flex items-center justify-center p-1 rounded-full cursor-pointer bg-[var(--gray-700)] active:bg-[var(--gray-800)] transition"
       >
-        <img
-          src={user.photoURL || placeholder}
-          alt="profile"
-          draggable={false}
-          className="w-14 h-14 rounded-full object-cover"
-        />
+        <ImageLoader imgSrc={user.photoURL} className="w-14 h-14 rounded-full object-cover" />
       </button>
 
       {/* Dropdown */}
@@ -86,11 +80,14 @@ export const UserProfile = ({ user }: { user: User }) => {
 
       {confirmLogOut && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(24,24,27,0.7)] px-4 sm:px-0 overflow-y-auto">
-          <div className="bg-[var(--gray-800)] text-[var(--gray-100)] rounded-xl p-4 sm:p-6 w-full max-w-md mx-auto my-10 shadow-lg">
+          <div
+            ref={confirmLogOutRef}
+            className="bg-[var(--gray-800)] text-[var(--gray-100)] rounded-xl p-4 sm:p-6 w-full max-w-md mx-auto my-10 shadow-lg"
+          >
             <p className="text-sm text-[var(--gray-400)] mb-4">Are you sure to logout?</p>
             <div className="flex flex-col sm:flex-row sm:space-x-3 space-y-2 sm:space-y-0">
               <button
-                onClick={() => setConfirmLogOut(false)}
+                onClick={handleLogOutPopup}
                 className="bg-[var(--gray-700)] hover:bg-[var(--gray-600)] cursor-pointer px-4 py-2 rounded-md transition"
               >
                 Cancel
@@ -107,4 +104,4 @@ export const UserProfile = ({ user }: { user: User }) => {
       )}
     </div>
   );
-};
+});
