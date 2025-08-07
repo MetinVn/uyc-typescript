323cf4b726210537f59effcf3f7ecaa33c413bd4
Meto <misaxanli@gmail.com>
2025-08-07 23:54:14 +0400

    performance optimization

--- src/components/reused-ui/reused-input.tsx
+++ src/components/reused-ui/reused-input.tsx
@@ -1,9 +1,11 @@
+import { memo } from "react";
+
 type CustomInputProps = {
   onValueChange: (value: string) => void;
   value: string;
 };
 
-export const CustomInput = ({ onValueChange, value }: CustomInputProps) => (
+export const CustomInput = memo(({ onValueChange, value }: CustomInputProps) => (
   <input
     type="text"
     name="musicLink"
@@ -12,4 +14,4 @@ export const CustomInput = ({ onValueChange, value }: CustomInputProps) => (
     onChange={(e) => onValueChange(e.target.value)}
     className="bg-[var(--gray-700)] text-[var(--gray-100)] text-sm w-full py-3 px-1 sm:p-3 border border-[var(--gray-600)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-500)] rounded-md placeholder-[var(--gray-400)] transition"
   />
-);
+));
