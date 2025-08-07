12eb6961ac414fff916b7900146bcf6efa1bd0d4
Meto <misaxanli@gmail.com>
2025-08-07 23:53:11 +0400

    performance optimization && passed a ref to close the modal on outside click

--- src/components/reused-ui/reused-form-modal.tsx
+++ src/components/reused-ui/reused-form-modal.tsx
@@ -1,8 +1,10 @@
+import { memo } from "react";
 import { ButtonState } from "../../types/types-animating-button";
-import AnimatingButton from "./reused-animating-button";
+import { AnimatingButton } from "./reused-animating-button";
 
 interface IFormModal {
   children: React.ReactNode;
+  ref: React.RefObject<HTMLDivElement | null>;
   title: string;
   dangerTitle?: boolean;
   confirmMessage?: string | null;
@@ -13,35 +15,41 @@ interface IFormModal {
   onSubmit: (e: React.FormEvent<HTMLElement>) => void;
 }
 
-export const FormModal = ({
-  children,
-  title,
-  dangerTitle = false,
-  confirmMessage,
-  buttonState,
-  onCloseModal,
-  animButtonText,
-  animButtonId,
-  onSubmit,
-}: IFormModal) => {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(24,24,27,0.7)] px-4 sm:px-0 overflow-y-auto">
-      <div className="bg-[var(--gray-800)] text-[var(--gray-100)] rounded-xl p-4 sm:p-6 w-full max-w-md mx-auto my-10 shadow-lg">
-        <h3 className={`text-lg font-semibold mb-4 ${dangerTitle ? "text-[var(--red-500)]" : ""}`}>{title}</h3>
-        {confirmMessage && <p className="text-sm text-[var(--gray-400)] mb-4">{confirmMessage}</p>}
-        {children}
-        <div className="flex flex-col sm:flex-row sm:space-x-3 space-y-2 sm:space-y-0 mt-4">
-          <button
-            type="button"
-            disabled={buttonState !== "default"}
-            onClick={onCloseModal}
-            className="bg-[var(--gray-700)] hover:bg-[var(--gray-600)] text-[var(--gray-100)] hover:text-[var(--accent-500)] cursor-pointer px-4 py-2 rounded-md transition"
-          >
-            Cancel
-          </button>
-          <AnimatingButton fullWidth defaultText={animButtonText} id={animButtonId} setButtonState={onSubmit} />
+export const FormModal = memo(
+  ({
+    children,
+    ref,
+    title,
+    dangerTitle = false,
+    confirmMessage,
+    buttonState,
+    onCloseModal,
+    animButtonText,
+    animButtonId,
+    onSubmit,
+  }: IFormModal) => {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(24,24,27,0.7)] px-4 sm:px-0 overflow-y-auto">
+        <div
+          ref={ref}
+          className="bg-[var(--gray-800)] text-[var(--gray-100)] rounded-xl p-4 sm:p-6 w-full max-w-md my-10 shadow-lg"
+        >
+          <h3 className={`text-lg font-semibold mb-4 ${dangerTitle ? "text-[var(--red-500)]" : ""}`}>{title}</h3>
+          {confirmMessage && <p className="text-sm text-[var(--gray-400)] mb-4">{confirmMessage}</p>}
+          {children}
+          <div className="flex flex-col sm:flex-row sm:space-x-3 space-y-2 sm:space-y-0 mt-4">
+            <button
+              type="button"
+              disabled={buttonState !== "default"}
+              onClick={onCloseModal}
+              className="bg-[var(--gray-700)] hover:bg-[var(--gray-600)] text-[var(--gray-100)] hover:text-[var(--accent-500)] cursor-pointer px-4 py-2 rounded-md transition"
+            >
+              Cancel
+            </button>
+            <AnimatingButton fullWidth defaultText={animButtonText} id={animButtonId} setButtonState={onSubmit} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
