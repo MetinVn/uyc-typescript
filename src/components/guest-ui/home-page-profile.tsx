5dc966695832192bdc291368387e96d0dd007c8e
Meto <misaxanli@gmail.com>
2025-08-09 11:24:33 +0400

    splitting code for performance

--- src/components/guest-ui/home-page-profile.tsx
+++ src/components/guest-ui/home-page-profile.tsx
@@ -1,20 +1,12 @@
-import { memo, useRef, useState } from "react";
+import { lazy, memo, Suspense, useRef, useState } from "react";
 import placeholder from "../../images/kitty-reduced.webp";
-import { ROUTES } from "../../routes/routes";
-import { CustomLink } from "../reused-ui/reused-router-link";
 import { useHandleOutsideClicks } from "../../hooks/hook-outside-clicks";
 
 export const GuestProfile = memo(() => {
   const [expand, setExpand] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const handleToggle = () => setExpand((prev) => !prev);
-
-  const featuresToUnlock = [
-    { icon: "★", label: "Favorite songs" },
-    { icon: "✩", label: "Rate songs" },
-    { icon: "♫", label: "Playlists" },
-    { icon: "🎬", label: "Convert to MP4" },
-  ];
+  const LazyGuestProfileDropdown = lazy(() => import("./home-page-profile-dropdown"));
 
   useHandleOutsideClicks({ isActive: expand, ref: dropdownRef, stateChanger: setExpand });
 
@@ -42,36 +34,17 @@ export const GuestProfile = memo(() => {
         <img src={placeholder} alt="profile" draggable={false} className="w-14 h-14 rounded-full object-cover" />
       </button>
 
-      {/* Dropdown */}
-      <div
-        className={`${
-          expand ? "translate-y-4 opacity-100 visible" : "translate-y-0 opacity-0 invisible"
-        } absolute right-1/2 translate-x-4 sm:translate-x-1/2 top-full min-w-55 w-full p-4 rounded-xl z-20 transition-all bg-[var(--gray-800)]`}
-      >
-        <div className="flex flex-col gap-1 text-[var(--gray-100)]">
-          {/* Welcome section */}
-          <div className="text-center text-xs">
-            <p className="font-semibold">&#128075; Welcome!</p>
-            <p>Sign in to access these features:</p>
-          </div>
-
-          {/* Feature List */}
-          <ul className="space-y-1 text-xs">
-            {featuresToUnlock.map((item, index) => (
-              <li key={index} className="flex items-center gap-2 pl-1">
-                <span className="w-4 text-center">{item.icon}</span>
-                <span>{item.label}</span>
-              </li>
-            ))}
-          </ul>
-
-          {/* Action buttons */}
-          <div className="flex flex-col font-medium items-center text-center gap-1">
-            <CustomLink path={ROUTES.AUTH.SIGN_IN} title="Sign in" />
-            <CustomLink path={ROUTES.AUTH.SIGN_UP} title="Create Account" />
-          </div>
-        </div>
-      </div>
+      {expand && (
+        <Suspense
+          fallback={
+            <div className="absolute text-center text-white inset-0 -translate-x-30 sm:translate-x-0 translate-y-20 w-full min-w-40 h-20 p-4 rounded-xl z-20 bg-[var(--gray-800)]">
+              Loading...
+            </div>
+          }
+        >
+          <LazyGuestProfileDropdown />
+        </Suspense>
+      )}
     </div>
   );
 });
