797fa080630502e3fbcea3cf48334ef2ef7269ad
Matin <misaxanli@gmail.com>
2026-02-28 20:26:01 +0100

    feat: add ErrorBoundary component to handle errors in the application

---
+++ src/error-boundary.tsx
@@ -0,0 +1,33 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+import Error from "./pages/Error/error";
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <Error />;
+    }
+
+    return this.props.children;
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,39 +2,56 @@ import { lazy } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ErrorBoundary } from "./error-boundary.tsx";
 
 import { ROUTES } from "./routes/routes.ts";
-import { Notifications } from "./components/notifications.tsx";
 import { UserProvider } from "./contexts/context-user.tsx";
-import { AuthInitializer, GuestPages, UserPages } from "./components/access-guards.tsx";
+import { Notifications } from "./components/notifications.tsx";
+import {
+  AuthInitializer,
+  GuestPages,
+  UserPages,
+} from "./components/access-guards.tsx";
 
 const HomePage = lazy(() => import("./pages/Home/home-page.tsx"));
-const RouteNotFoundPage = lazy(() => import("./pages/NotFound/route-not-found.tsx"));
+const RouteNotFoundPage = lazy(
+  () => import("./pages/NotFound/route-not-found.tsx"),
+);
 const AccountPage = lazy(() => import("./pages/Account/account-page.tsx"));
 const MusicPage = lazy(() => import("./pages/Music/music-chunk.tsx"));
-const ResetPasswordPage = lazy(() => import("./pages/ResetPassword/reset-password-page.tsx"));
+const ResetPasswordPage = lazy(
+  () => import("./pages/ResetPassword/reset-password-page.tsx"),
+);
 const SignInPage = lazy(() => import("./pages/SignIn/signin-page.tsx"));
 const SignUpPage = lazy(() => import("./pages/SignUp/signup-page.tsx"));
 
 createRoot(document.getElementById("root")!).render(
   <Router>
-    <Notifications />
-    <UserProvider>
-      <Routes>
-        <Route index path={ROUTES.HOME} element={<HomePage />} />
-        <Route path={ROUTES.ROUTE_NOT_FOUND} element={<RouteNotFoundPage />} />
-        <Route path={ROUTES.FORGOT_PASSWORD} element={<ResetPasswordPage />} />
-        <Route element={<AuthInitializer />}>
-          <Route element={<GuestPages />}>
-            <Route path={ROUTES.AUTH.SIGN_IN} element={<SignInPage />} />
-            <Route path={ROUTES.AUTH.SIGN_UP} element={<SignUpPage />} />
+    <ErrorBoundary>
+      <Notifications />
+      <UserProvider>
+        <Routes>
+          <Route index path={ROUTES.HOME} element={<HomePage />} />
+          <Route
+            path={ROUTES.ROUTE_NOT_FOUND}
+            element={<RouteNotFoundPage />}
+          />
+          <Route
+            path={ROUTES.FORGOT_PASSWORD}
+            element={<ResetPasswordPage />}
+          />
+          <Route element={<AuthInitializer />}>
+            <Route element={<GuestPages />}>
+              <Route path={ROUTES.AUTH.SIGN_IN} element={<SignInPage />} />
+              <Route path={ROUTES.AUTH.SIGN_UP} element={<SignUpPage />} />
+            </Route>
+            <Route element={<UserPages />}>
+              <Route path={ROUTES.MUSIC} element={<MusicPage />} />
+              <Route path={ROUTES.AUTH.ACCOUNT} element={<AccountPage />} />
+            </Route>
           </Route>
-          <Route element={<UserPages />}>
-            <Route path={ROUTES.MUSIC} element={<MusicPage />} />
-            <Route path={ROUTES.AUTH.ACCOUNT} element={<AccountPage />} />
-          </Route>
-        </Route>
-      </Routes>
-    </UserProvider>
-  </Router>
+        </Routes>
+      </UserProvider>
+    </ErrorBoundary>
+  </Router>,
 );
