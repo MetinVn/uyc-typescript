b820cf34d9f8fe446364eb5e70c82cd5a0452333
Matin <misaxanli@gmail.com>
2026-03-07 18:34:01 +0100

    unimplemented feature

--- src/components/guest-ui/home-page-dropdown-skeleton.tsx
+++ src/components/guest-ui/home-page-dropdown-skeleton.tsx
@@ -11,7 +11,7 @@ export const ProfileLoader = () => {
 
       {/* Feature list loader */}
       <ul className="space-y-2 p-1">
-        {[...Array(4)].map((_, index) => (
+        {[...Array(3)].map((_, index) => (
           <li key={index} className="flex items-center gap-2">
             <Skeleton className="h-[12px] w-[12px] rounded-full bg-[var(--gray-700)]" />
             <Skeleton className="h-[10px] w-[100px] rounded-md bg-[var(--gray-700)]" />
